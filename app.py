2b39cf38de575110340bb2249a46be18c154a929
Fahimmuntasir1 <fahimmuntasir043@gamil.com>
2024-05-04 11:15:42 -0700

    weather app is working successfully

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ import requests
 
 api_key = "66c7de2faaa5e5c7252ac24458a454de"
 user_input = input("Enter a city: ")
-url = f"http://api.openweathermap.org/data/2.5/weather?q={user_input}&appid={api_key}"
+weather_data = (
+    f"http://api.openweathermap.org/data/2.5/weather?q={user_input}&appid={api_key}"
+)
 
-response = requests.get(url)
-print(response)
+response = requests.get(weather_data)
+print(response.json())
+if response.status_code == 200:
+    data = response.json()
+    temperature = data["main"]["temp"]
+    description = data["weather"][0]["description"]
+    country = data["sys"]["country"]
+    print(f"Temperature: {temperature} K")
+    print(f"Description: {description}")
+    print(f"Country: {country}")
+else:
+    print("Error fetching weather data")
