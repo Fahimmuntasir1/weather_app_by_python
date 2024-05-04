22664bad045de521de9e21335b2b7d7e690f11d2
Fahimmuntasir1 <fahimmuntasir043@gamil.com>
2024-05-04 10:54:28 -0700

    response is 200 app working

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-# Weather app
+# Creating a weather app using python
 # - - - - - - - - - - -- - ----------->>
+import requests
+
+api_key = "66c7de2faaa5e5c7252ac24458a454de"
+user_input = input("Enter a city: ")
+url = f"http://api.openweathermap.org/data/2.5/weather?q={user_input}&appid={api_key}"
+
+response = requests.get(url)
+print(response)
